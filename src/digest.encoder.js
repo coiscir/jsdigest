394ffbb0ceb116e4b4c86e090ca4fa01f098a660
Jonathan Lonowski <coiscir@gmail.com>
2009-08-09 13:30:56 -0500

    Add RFC-3548 encoding methods to Digest.Encoder.

--- src/digest.encoder.js
+++ src/digest.encoder.js
@@ -120,6 +120,141 @@
     };
     
     
+    /** RFC-3548 - Base-16 **/
+    
+    function toBase16(chars) {
+      var i = 0, str = '',
+        arr = chars.split('');
+      
+      for (; i < sequence.length; i += 1) {
+        str += arr[(sequence[i] >> 4) & 0xf] || '?';
+        str += arr[(sequence[i] >> 0) & 0xf] || '?';
+      }
+      return str;
+    }
+    
+    // 0-9 a-f (lower)
+    this.hex = function hex() {
+      return toBase16('0123456789abcdef');
+    };
+    
+    // 0-9 A-F (upper)
+    this.base16 = function base16() {
+      return toBase16('0123456789ABCDEF');
+    };
+    
+    
+    /** RFC-3548 - Base-32 **/
+    
+    function toBase32(chars) {
+      var i = 0, str = '', rem = null,
+        padlen = 8, padding = '=',
+        arr = chars.split('');
+      
+      for (; i < sequence.length; i += 1) {
+        switch (i % 5) {
+        case 0:
+          // 00000000 xxxxx000
+          str += arr[((sequence[i] >> 3) & 0x1f) | 0x0] || '?';
+          rem = (sequence[i] & 0x07) << 2;
+          break;
+        case 1:
+          // 00000rrr xxyyyyy0
+          str += arr[((sequence[i] >> 6) & 0x03) | rem] || '?';
+          str += arr[((sequence[i] >> 1) & 0x1f) | 0x0] || '?';
+          rem = (sequence[i] & 0x01) << 4;
+          break;
+        case 2:
+          // 0000000r xxxx0000
+          str += arr[((sequence[i] >> 4) & 0x0f) | rem] || '?';
+          rem = (sequence[i] & 0x0f) << 1;
+          break;
+        case 3:
+          // 0000rrrr xyyyyy00
+          str += arr[((sequence[i] >> 7) & 0x01) | rem] || '?';
+          str += arr[((sequence[i] >> 2) & 0x1f) | 0x0] || '?';
+          rem = (sequence[i] & 0x03) << 3;
+          break;
+        case 4:
+          // 000000rr xxxyyyyy
+          str += arr[((sequence[i] >> 5) & 0x07) | rem] || '?';
+          str += arr[((sequence[i] >> 0) & 0x1f) | 0x0] || '?';
+          rem = null;
+        }
+      }
+      
+      // append remainder
+      str += (null === rem) ? '' : (arr[rem] || '?');
+      
+      // append padding
+      while (padlen > 0 && (str.length % padlen) > 0) {
+        str += padding;
+      }
+      
+      return str;
+    }
+    
+    // A-Z 2-7 (upper)
+    this.base32 = function base32() {
+      return toBase32('ABCDEFGHIJKLMNOPQRSTUVWXYZ234567');
+    };
+    
+    // 0-9 a-v (lower)
+    this.base32hex = function base32hex() {
+      return toBase32('0123456789abcdefghijklmnopqrstuv');
+    };
+    
+    
+    /** RFC-3548 - Base-64 **/
+    
+    function toBase64(chars) {
+      var i = 0, str = '', rem = null,
+        padlen = 4, padding = '=',
+        arr = chars.split('');
+      
+      for (; i < sequence.length; i += 1) {
+        switch (i % 3) {
+        case 0:
+          // 00000000 xxxxxx00
+          str += arr[((sequence[i] >> 2) & 0x3f) | 0x0] || '?';
+          rem = (sequence[i] & 0x03) << 4;
+          break;
+        case 1:
+          // 000000rr xxxx0000
+          str += arr[((sequence[i] >> 4) & 0x0f) | rem] || '?';
+          rem = (sequence[i] & 0x0f) << 2;
+          break;
+        case 2:
+          // 0000rrrr xxyyyyyy
+          str += arr[((sequence[i] >> 6) & 0x03) | rem] || '?';
+          str += arr[((sequence[i] >> 0) & 0x3f) | 0x0] || '?';
+          rem = null;
+          break;
+        }
+      }
+      
+      // append remainder
+      str += (null === rem) ? '' : (arr[rem] || '?');
+      
+      // append padding
+      while (padlen > 0 && (str.length % padlen) > 0) {
+        str += padding;
+      }
+      
+      return str;
+    }
+    
+    // A-Z a-z 0-9 + /
+    this.base64 = function base64() {
+      return toBase64('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/');
+    };
+    
+    // A-Z a-z 0-9 - _ (url/filename safe)
+    this.base64url = function base64url() {
+      return toBase64('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_');
+    };
+    
+    
     /** Overrides **/
     
     this.valueOf = function valueOf() {
