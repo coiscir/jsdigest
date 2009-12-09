db9fb8b89e889ce5ddb9326dc80796fab9bfb094
Jonathan Lonowski <coiscir@gmail.com>
2009-12-08 19:14:47 -0600

    Remove Digest.configure.

--- src/global.js
+++ src/global.js
@@ -7,21 +7,6 @@
   this.Digest = new function Digest() {
     this.Version = '<%= version %>';
     this.fn = this.constructor.prototype;
-    
-    this.configure = function configure(fn, cfg) {
-      if ('function' === typeof fn) {
-        var block = cfg.block || 0,  // block size in bytes
-            curri = cfg.curri || 0,  // arguments index to insert data
-            curry = cfg.curry || []; // unfinished arguments array
-        
-        fn.block = block;
-        fn.curry = function (data) {
-          var args = [].concat(curry);
-          args[curri] = data;
-          return fn.apply(null, args);
-        };
-      }
-    };
   }();
   
 }());
