0302cc461cd778669dbc83ded81fe8e6403c6f14
Jonathan Lonowski <coiscir@gmail.com>
2009-12-11 00:52:17 -0600

    Fix function name and add missing else block in HMAC.

--- src/hmac.js
+++ src/hmac.js
@@ -12,6 +12,8 @@
     
     if (hkey.length > block) {
       akey = hash(hkey).trunc();
+    } else {
+      akey = self.Encoder(hkey).trunc();
     }
     
     for (i = 0, ipad = [], opad = []; i < block; i += 1) {
@@ -19,7 +21,7 @@
       opad[i] = (akey[i] || 0x00) ^ 0x5c;
     }
     
-    return hash(opad.concat(hash(ipad.concat(data)).trunk()));
+    return hash(opad.concat(hash(ipad.concat(data)).trunc()));
   };
   
 }(Digest));
