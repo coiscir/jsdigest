1b28fb2bff7e6fbc21f9d093927228c4633f263f
Jonathan Lonowski <coiscir@gmail.com>
2009-12-10 21:30:20 -0600

    Simplify HMAC since it should no longer be used directly.

--- src/digest.js
+++ src/digest.js
@@ -6,4 +6,4 @@
 
 "use strict";
 
-<%= import('global.js', 'encoder.js') %>
+<%= import('global.js', 'encoder.js', 'hmac.js') %>
---
+++ src/hmac.js
@@ -0,0 +1,25 @@
+/**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  HMAC - keyed-Hash Message Authentication Code
+**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~**/
+
+(function HMAC(self) {
+  
+  self.fn.hmac = function hmac(hash, data, hkey, block) {
+    var i, akey, ipad, opad;
+    
+    data = self.Encoder(data).trunc();
+    hkey = self.Encoder(hkey).trunc();
+    
+    if (hkey.length > block) {
+      akey = hash(hkey).trunc();
+    }
+    
+    for (i = 0, ipad = [], opad = []; i < block; i += 1) {
+      ipad[i] = (akey[i] || 0x00) ^ 0x36;
+      opad[i] = (akey[i] || 0x00) ^ 0x5c;
+    }
+    
+    return hash(opad.concat(hash(ipad.concat(data)).trunk()));
+  };
+  
+}(Digest));
