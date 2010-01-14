9a6af08277339971df571b9c364d024cb68522f8
Jonathan Lonowski <coiscir@gmail.com>
2010-01-14 02:57:40 -0600

    Update copyright to 2010.

--- src/intro.js
+++ src/intro.js
@@ -2,7 +2,7 @@
  *  jsDigest v@VERSION (@RELEASE)
  *  http://github.com/coiscir/jsdigest/
  *
- *  Copyright (c) 2009 Jonathan Lonowski
+ *  Copyright (c) 2010 Jonathan Lonowski
  *  Released and distributed under the MIT License.
 **/
 
