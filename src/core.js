ac776005143881ab376a513aa85a886c10cc5dbc
Jonathan Lonowski <coiscir@gmail.com>
2011-03-07 00:33:20 -0600

    Reformat word.js, add as import.

--- src/core.js
+++ src/core.js
@@ -11,7 +11,7 @@
 var self = { Version: '<%= @version %>' };
 
 
-<%= import 'math' %>
+<%= import 'math', 'word' %>
 
 
 /* Export */
--- src/word.js
+++ src/word.js
@@ -1,112 +1,145 @@
 // Least Significant Byte, 32-bit
-function merge_LSB_32(input) {
-  var i, j, l, output = [];
-  for (i = 0, j = 0, l = input.length; j < l; i += 1, j = (i * 4)) {
-    output[i] = ((input[j]) & 0xff) |
-      ((input[j + 1] <<  8) & 0xff00) |
-      ((input[j + 2] << 16) & 0xff0000) |
-      ((input[j + 3] << 24) & 0xff000000);
+function mergeLeast_32( input ) {
+  var i,
+      length = input.length,
+      output = [];
+
+  for ( i = 0; i < length; i += 4 ) {
+    output.push(
+      ( ( input[ i + 0 ] & 0xff ) <<  0 ) |
+      ( ( input[ i + 1 ] & 0xff ) <<  8 ) |
+      ( ( input[ i + 2 ] & 0xff ) << 16 ) |
+      ( ( input[ i + 3 ] & 0xff ) << 24 )
+    );
   }
+
   return output;
 }
 
-function split_LSB_32(input) {
-  var i, l, output = [];
-  for (i = 0, l = input.length; i < l; i += 1) {
-    output.push((input[i] >>  0) & 0xff);
-    output.push((input[i] >>  8) & 0xff);
-    output.push((input[i] >> 16) & 0xff);
-    output.push((input[i] >> 24) & 0xff);
+function splitLeast_32( input ) {
+  var i,
+      length = input.length,
+      output = [];
+
+  for ( i = 0; i < length; i += 1 ) {
+    output.push( ( input[i] >>  0 ) & 0xff );
+    output.push( ( input[i] >>  8 ) & 0xff );
+    output.push( ( input[i] >> 16 ) & 0xff );
+    output.push( ( input[i] >> 24 ) & 0xff );
   }
+
   return output;
 }
 
 // Most Significant Byte, 32-bit
-function merge_MSB_32(input) {
-  var i, j, l, output = [];
-  for (i = 0, j = 0, l = input.length; j < l; i += 1, j = (i * 4)) {
-    output[i] = 
-      ((input[j + 0] & 0xff) << 24) |
-      ((input[j + 1] & 0xff) << 16) |
-      ((input[j + 2] & 0xff) <<  8) |
-      ((input[j + 3] & 0xff) <<  0);
+function mergeMost_32( input ) {
+  var i,
+      length = input.length,
+      output = [];
+
+  for ( i = 0; i < length; i += 4 ) {
+    output.push(
+      ( ( input[ i + 0 ] & 0xff ) << 24 ) |
+      ( ( input[ i + 1 ] & 0xff ) << 16 ) |
+      ( ( input[ i + 2 ] & 0xff ) <<  8 ) |
+      ( ( input[ i + 3 ] & 0xff ) <<  0 )
+    );
   }
+
   return output;
 }
 
-function split_MSB_32(input) {
-  var i, l, output = [];
-  for (i = 0, l = input.length; i < l; i += 1) {
-    output.push((input[i] >> 24) & 0xff);
-    output.push((input[i] >> 16) & 0xff);
-    output.push((input[i] >>  8) & 0xff);
-    output.push((input[i] >>  0) & 0xff);
+function splitMost_32( input ) {
+  var i,
+      length = input.length,
+      output = [];
+
+  for ( i = 0; i < length; i += 1 ) {
+    output.push( ( input[i] >> 24 ) & 0xff );
+    output.push( ( input[i] >> 16 ) & 0xff );
+    output.push( ( input[i] >>  8 ) & 0xff );
+    output.push( ( input[i] >>  0 ) & 0xff );
   }
+
   return output;
 }
 
 // Least Significant Byte, 64-bit
-function merge_LSB_64(input) {
-  var i, j, l, output = [];
-  for (i = 0, j = 0, l = input.length; j < l; i += 1, j = (i * 8)) {
-    output[i] = [
-      ((input[j + 4] & 0xff) <<  0) |
-      ((input[j + 5] & 0xff) <<  8) |
-      ((input[j + 6] & 0xff) << 16) |
-      ((input[j + 7] & 0xff) << 24),
-      ((input[j + 0] & 0xff) <<  0) |
-      ((input[j + 1] & 0xff) <<  8) |
-      ((input[j + 2] & 0xff) << 16) |
-      ((input[j + 3] & 0xff) << 24)
-    ];
+function mergeLeast_64( input ) {
+  var i,
+      length = input.length,
+      output = [];
+
+  for ( i = 0; i < length; i += 8 ) {
+    output.push([
+      ( ( input[ i + 4 ] & 0xff ) <<  0 ) |
+      ( ( input[ i + 5 ] & 0xff ) <<  8 ) |
+      ( ( input[ i + 6 ] & 0xff ) << 16 ) |
+      ( ( input[ i + 7 ] & 0xff ) << 24 ),
+      ( ( input[ i + 0 ] & 0xff ) <<  0 ) |
+      ( ( input[ i + 1 ] & 0xff ) <<  8 ) |
+      ( ( input[ i + 2 ] & 0xff ) << 16 ) |
+      ( ( input[ i + 3 ] & 0xff ) << 24 )
+    ]);
   }
+
   return output;
 }
 
-function split_LSB_64(input) {
-  var i, l, output = [];
-  for (i = 0, l = input.length; i < l; i += 1) {
-    output.push((input[i][1] >>  0) & 0xff);
-    output.push((input[i][1] >>  8) & 0xff);
-    output.push((input[i][1] >> 16) & 0xff);
-    output.push((input[i][1] >> 24) & 0xff);
-    output.push((input[i][0] >>  0) & 0xff);
-    output.push((input[i][0] >>  8) & 0xff);
-    output.push((input[i][0] >> 16) & 0xff);
-    output.push((input[i][0] >> 24) & 0xff);
+function splitLeast_64( input ) {
+  var i,
+      length = input.length,
+      output = [];
+
+  for ( i = 0; i < length; i += 1 ) {
+    output.push( ( input[i][1] >>  0 ) & 0xff );
+    output.push( ( input[i][1] >>  8 ) & 0xff );
+    output.push( ( input[i][1] >> 16 ) & 0xff );
+    output.push( ( input[i][1] >> 24 ) & 0xff );
+    output.push( ( input[i][0] >>  0 ) & 0xff );
+    output.push( ( input[i][0] >>  8 ) & 0xff );
+    output.push( ( input[i][0] >> 16 ) & 0xff );
+    output.push( ( input[i][0] >> 24 ) & 0xff );
   }
+
   return output;
 }
 
 // Most Significant Byte, 64-bit
-function merge_MSB_64(input) {
-  var i, j, l, output = [];
-  for (i = 0, j = 0, l = input.length; j < l; i += 1, j = (i * 8)) {
-    output[i] = [
-      ((input[j + 0] & 0xff) << 24) |
-      ((input[j + 1] & 0xff) << 16) |
-      ((input[j + 2] & 0xff) <<  8) |
-      ((input[j + 3] & 0xff) <<  0),
-      ((input[j + 4] & 0xff) << 24) |
-      ((input[j + 5] & 0xff) << 16) |
-      ((input[j + 6] & 0xff) <<  8) |
-      ((input[j + 7] & 0xff) <<  0)
-    ];
+function mergeMost_64( input ) {
+  var i,
+      length = input.length,
+      output = [];
+
+  for ( i = 0; i < length; i += 8 ) {
+      ( ( input[ i + 0 ] & 0xff ) << 24 ) |
+      ( ( input[ i + 1 ] & 0xff ) << 16 ) |
+      ( ( input[ i + 2 ] & 0xff ) <<  8 ) |
+      ( ( input[ i + 3 ] & 0xff ) <<  0 ),
+      ( ( input[ i + 4 ] & 0xff ) << 24 ) |
+      ( ( input[ i + 5 ] & 0xff ) << 16 ) |
+      ( ( input[ i + 6 ] & 0xff ) <<  8 ) |
+      ( ( input[ i + 7 ] & 0xff ) <<  0 )
   }
+  
   return output;
 }
 
-function split_MSB_64(input) {
-  var i, l, output = [];
-  for (i = 0, l = input.length; i < l; i += 1) {
-    output.push((input[i][0] >> 24) & 0xff);
-    output.push((input[i][0] >> 16) & 0xff);
-    output.push((input[i][0] >>  8) & 0xff);
-    output.push((input[i][0] >>  0) & 0xff);
-    output.push((input[i][1] >> 24) & 0xff);
-    output.push((input[i][1] >> 16) & 0xff);
-    output.push((input[i][1] >>  8) & 0xff);
-    output.push((input[i][1] >>  0) & 0xff);
+function splitMost_64( input ) {
+  var i,
+      length = input.length,
+      output = [];
+
+  for ( i = 0; i < length; i += 1 ) {
+    output.push( ( input[i][0] >> 24 ) & 0xff );
+    output.push( ( input[i][0] >> 16 ) & 0xff );
+    output.push( ( input[i][0] >>  8 ) & 0xff );
+    output.push( ( input[i][0] >>  0 ) & 0xff );
+    output.push( ( input[i][1] >> 24 ) & 0xff );
+    output.push( ( input[i][1] >> 16 ) & 0xff );
+    output.push( ( input[i][1] >>  8 ) & 0xff );
+    output.push( ( input[i][1] >>  0 ) & 0xff );
   }
+
   return output;
 }
