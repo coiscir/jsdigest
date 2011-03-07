29ee79e1674015492596deba1f72d3fc8af9cd1c
Jonathan Lonowski <coiscir@gmail.com>
2011-03-07 02:33:09 -0600

    Revise SHA-1, adding as import.

--- src/hash/sha1.js
+++ src/hash/sha1.js
@@ -1,11 +1,12 @@
 // SHA-1 (c) 2006 The Internet Society
 (function () {
-  function main(size, data) {
-    var a, b, c, d, e, i, l, r, t, tmp, w, x,
-      bytes, bitHi, bitLo,
-      padlen, padding = [0x80],
-      hash = [0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476, 0xc3d2e1f0],
-      K = [0x5a827999, 0x6ed9eba1, 0x8f1bbcdc, 0xca62c1d6],
+  var merge = mergeMost_32,
+      split = splitMost_32,
+      rotl = rotl_32,
+      
+      DIGEST = 160,
+      BLOCK = 64,
+      K = [ 0x5a827999, 0x6ed9eba1, 0x8f1bbcdc, 0xca62c1d6 ],
       F = [
         function (b, c, d) {
           return (b & c) | ((~b) & d);
@@ -20,39 +21,40 @@
           return (b ^ c ^ d);
         }
       ];
+      
+  function sha1( data ) {
+    var a, b, c, d, e, i, l, r, t, tmp, w, x,
+        bytes = data.length,
+        padding = [ 0x80 ],
+        hash = [ 0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476, 0xc3d2e1f0 ];
     
-    // use bit-length to pad data
-    bytes = data.length;
-    bitLo = (bytes * 8) & 0xffffffff;
-    bitHi = (bytes * 8 / Math.pow(2, 32)) & 0xffffffff;
+    padding.length = ( ( bytes % 64 ) < 56 ? 56 : 120 ) - ( bytes % 64 );
     
-    padlen = ((bytes % 64) < 56 ? 56 : 120) - (bytes % 64);
-    while (padding.length < padlen) {
-      padding.push(0x0);
-    }
-    
-    x = merge_MSB_32(data.concat(padding)).concat([bitHi, bitLo]);
+    x = merge( data.concat( padding ) ).concat([
+      ( bytes * 8 / Math.pow( 2, 32 ) ) | 0x0,
+      ( bytes * 8 ) | 0x0
+    ]);
     
     // update hash
-    for (i = 0, w = [], l = x.length; i < l; i += 16) {
+    for ( i = 0, w = [], l = x.length; i < l; i += 16 ) {
       a = hash[0];
       b = hash[1];
       c = hash[2];
       d = hash[3];
       e = hash[4];
       
-      for (t = 0; t < 80; t += 1) {
-        if (t < 16) {
-          w[t] = x[i + t];
+      for ( t = 0; t < 80; t += 1 ) {
+        if ( t < 16 ) {
+          w[t] = x[ i + t ];
         } else {
-          w[t] = rotl_32((w[t - 3] ^ w[t - 8] ^ w[t - 14] ^ w[t - 16]), 1);
+          w[t] = rotl( w[t - 3] ^ w[t - 8] ^ w[t - 14] ^ w[t - 16], 1 );
         }
         
-        r = Math.floor(t / 20);
-        tmp = (rotl_32(a, 5) + F[r](b, c, d) + e + w[t] + K[r]);
+        r = Math.floor( t / 20 );
+        tmp = rotl( a, 5 ) + F[r]( b, c, d ) + e + w[t] + K[r];
         e = d;
         d = c;
-        c = rotl_32(b, 30);
+        c = rotl( b, 30 );
         b = a;
         a = tmp;
       }
@@ -64,30 +66,9 @@
       hash[4] += e;
     }
     
-    return self.Encoder(crop(size, split_MSB_32(hash), false));
+    return split( hash );
   }
   
-  // expose hash function
-  
-  self.sha1 = function sha1(size, data, hkey) {
-    var digest = 160;
-    
-    // allow size to be optional
-    if ('number' !== typeof size.valueOf()) {
-      hkey = data;
-      data = size;
-      size = digest;
-    }
-    
-    size = (0 < size && size <= digest) ? size : digest;
-    data = self.Encoder.ready(data);
-    hkey = self.Encoder.ready(hkey);
-    
-    if (isInput(hkey)) {
-      return hmac(main, size, digest, data, hkey, 64);
-    } else {
-      return main(size, data);
-    }
-  };
+  self.sha1 = factorMAC( hmac, sha1, DIGEST, BLOCK );
   
 }());
