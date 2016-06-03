f28f422e6287f9be52f82b234ecfbf8fbec35260
Jonathan Lonowski <coiscir@gmail.com>
2016-06-03 01:43:10 -0500

    Change to UMD, following `returnExportsGlobal` template.
    RE: #1 part 2.

--- src/digest.js
+++ src/digest.js
@@ -2,23 +2,29 @@
  *  jsDigest v<%= @version %> (<%= @release %>)
  *  http://github.com/coiscir/jsdigest/
  *
- *  Copyright (c) 2009-2011 Jonathan Lonowski
+ *  Copyright (c) 2009-2016 Jonathan Lonowski
  *  Released and distributed under the MIT License.
 **/
 
-(function () { "use strict";
+/* umdjs - returnExportsGlobal */
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define('jsdigest', function () {
+      return (root.Digest = factory());
+    });
+  } else if (typeof module === 'object' && module.exports) {
+    module.exports = factory();
+  } else {
+    root.Digest = factory();
+  }
+})(this, function () {
+
+'use strict';
 
 var self = { Version: '<%= @version %>' };
 
-
 <%= import 'core', 'encoder', 'math', 'word', 'hash/*' %>
 
+return self;
 
-/* Export */
-
-if ( 'undefined' === typeof exports )
-  window.Digest = self;
-else
-  module.exports = self;
-
-})();
+});
